0a14af37ab86efd491cc97c50db1abfb3a975c40
James Faix <jamesthefaix@gmail.com>
2022-01-03 14:24:56 -0800

    Refactoring

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
 import { Card } from '../model';
-import CardResultList from './card-results-list';
 import * as Scryfall from 'scryfall-api';
 import { DebounceInput } from 'react-debounce-input';
 
 async function queryCards(query: string) : Promise<Array<Card>> {
-    const pageLength = 175; // Cannot be changed
-
+    // const pageLength = 175; // Cannot be changed
     const cards = await Scryfall.Cards.search(query).all();
-
     return cards.map(c => { return { Name: c.name }; });
 }
 
@@ -34,7 +31,13 @@ const App = () =>
                     debounceTimeout={300}
                 />
             </div>
-            <CardResultList Cards={cards}/>
+            <div>
+                {cards.map((c, i) => { return (
+                    <div key={i.toString()}>
+                        {c.Name}
+                    </div>
+                );})}
+            </div>
         </div>
     );
 }
