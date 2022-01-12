f303ccdd9053b38dc2f88a77213553291a83901f
James Faix <jamesthefaix@gmail.com>
2022-01-12 11:54:45 -0800

    Added redux logger

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { applyMiddleware, combineReducers, createStore } from '@reduxjs/toolkit';
 import { currentSelectionReducer } from './currentSelection';
 import { encyclopediaReducer } from './encyclopedia';
 import { inventoryReducer } from './inventory';
+import logger from 'redux-logger';
 
-export const store = configureStore({
-    reducer: {
-        encyclopedia: encyclopediaReducer,
-        inventory: inventoryReducer,
-        currentSelection: currentSelectionReducer
-    }
+const reducer = combineReducers({
+    encyclopedia: encyclopediaReducer,
+    inventory: inventoryReducer,
+    currentSelection: currentSelectionReducer
 });
+
+export const store = createStore(
+    reducer,
+    applyMiddleware(logger)
+);
 
 export type RootState = ReturnType<typeof store.getState>
 
