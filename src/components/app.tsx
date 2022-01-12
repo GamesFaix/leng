8d2603638851da20efd1c6d33091236cbda64e14
James Faix <jamesthefaix@gmail.com>
2022-01-12 07:41:29 -0800

    Removed BulkDataFetcher component

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Card } from 'scryfall-api';
+import { loadCards } from '../logic/bulk-data-controller';
 import { CardName, toCardNames } from '../logic/model';
-import BulkDataFetcher from './bulk-data-fetcher';
 import CardSearch from './card-search';
 
 const App = () =>
@@ -9,18 +9,19 @@ const App = () =>
     const [cards, setCards] = React.useState<Card[]>([]);
     const [cardNames, setCardNames] = React.useState<CardName[]>([]);
 
+    React.useEffect(() => {
+        loadCards()
+            .then(() => {
+                console.log('setCards', cards);
+                setCards(cards);
+                const cardNames = toCardNames(cards);
+                console.log('setCardNames', cardNames);
+                setCardNames(cardNames);
+            });
+    });
+
     return (
         <div className='app'>
-            <BulkDataFetcher
-                cards={cards}
-                onLoad={cards => {
-                    console.log('setCards', cards);
-                    setCards(cards);
-                    const cardNames = toCardNames(cards);
-                    console.log('setCardNames', cardNames);
-                    setCardNames(cardNames);
-                }}
-            />
             <CardSearch
                 cards={cardNames}
                 onSelection={card => console.log(`selected ${card.name}`)}
