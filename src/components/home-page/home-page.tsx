b2a32cf16d1defc9c7e787a78ede71e15e61c478
James Faix <jamesthefaix@gmail.com>
2022-01-13 21:13:19 -0800

    Refactored new box form into separate file

--- src/components/home-page/home-page.tsx
+++ src/components/home-page/home-page.tsx
@@ -3,17 +3,17 @@ import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { icons } from '../../fontawesome';
-import { createBox, deleteBox, getBoxInfos } from '../../logic/inventoryController';
+import { deleteBox, getBoxInfos } from '../../logic/inventoryController';
 import { RootState } from '../../store';
 import { InventoryActionTypes } from '../../store/inventory';
 import BoxesTable from './boxes-table';
+import NewBoxForm from './new-box-form';
 
 const HomePage = () => {
     const settings = useSelector((state: RootState) => state.settings.settings);
     const boxes = useSelector((state: RootState) => state.inventory.boxes);
     const dispatch = useDispatch();
     const [isNewBoxFormVisible, setIsNewBoxFormVisible] = React.useState(false);
-    const [newBoxName, setNewBoxName] = React.useState('');
 
     React.useEffect(() => {
         if (settings !== null && boxes === null) {
@@ -28,74 +28,31 @@ const HomePage = () => {
         }
     });
 
-    function showNewBoxForm() {
-        setIsNewBoxFormVisible(true);
-        setNewBoxName('');
-    }
-
-    function hideNewBoxForm() {
-        setIsNewBoxFormVisible(false);
-        setNewBoxName('');
-    }
-
-    function createNewBox() {
-        createBox(settings, newBoxName, dispatch);
-        hideNewBoxForm();
-    }
-
-    function deleteBox1(name: string) {
-        deleteBox(settings, name, dispatch);
-    }
-
-    const boxLinks =
-        boxes === null
-        ? "Loading box info..."
-        : (<BoxesTable
-            boxes={boxes}
-            deleteBox={deleteBox1}
-        />);
-
-    const newBoxForm =
-        isNewBoxFormVisible
-        ? (<>
-            <div>
-                <input
-                    type="text"
-                    onChange={e => setNewBoxName(e.target.value)}
-                    value={newBoxName}
-                    placeholder="Enter box name..."
-                />
-                <button
-                    type="button"
-                    title="Create box"
-                    onClick={() => createNewBox()}
-                >
-                    <FontAwesomeIcon icon={icons.ok} />
-                </button>
-                <button
-                    type="button"
-                    title="Cancel"
-                    onClick={() => hideNewBoxForm()}
-                >
-                    <FontAwesomeIcon icon={icons.cancel} />
-                </button>
-            </div>
-            <br/>
-        </>)
-        : "";
-
     return (
         <div>
             <h2>Home</h2>
             <br/>
-            <button title="Add box"
-                onClick={() => showNewBoxForm()}
+            <button
+                title="Add box"
+                onClick={() => setIsNewBoxFormVisible(true)}
             >
                 <FontAwesomeIcon icon={icons.add} />
             </button>
             <br/>
-            {newBoxForm}
-            {boxLinks}
+            {isNewBoxFormVisible
+                ? (<>
+                    <NewBoxForm close={() => setIsNewBoxFormVisible(false)}/>
+                    <br/>
+                </>)
+                : ""
+            }
+            {boxes === null
+                ? "Loading box info..."
+                : (<BoxesTable
+                    boxes={boxes}
+                    deleteBox={name => deleteBox(settings, name, dispatch)}
+                />)
+            }
             <br/>
             <Link to="/settings">
                 <button title="Settings">
---
+++ src/components/home-page/new-box-form.tsx
@@ -0,0 +1,43 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { icons } from '../../fontawesome';
+import { createBox } from '../../logic/inventoryController';
+import { RootState } from '../../store';
+
+type Props = {
+    close: () => void
+}
+
+const NewBoxForm = (props: Props) => {
+    const settings = useSelector((state: RootState) => state.settings.settings);
+    const dispatch = useDispatch();
+    const [newBoxName, setNewBoxName] = React.useState('');
+
+    return (<div>
+        <input
+            type="text"
+            onChange={e => setNewBoxName(e.target.value)}
+            value={newBoxName}
+            placeholder="Enter box name..."
+        />
+        <button
+            type="button"
+            title="Create box"
+            onClick={() => {
+                createBox(settings, newBoxName, dispatch);
+                props.close();
+            }}
+        >
+            <FontAwesomeIcon icon={icons.ok} />
+        </button>
+        <button
+            type="button"
+            title="Cancel"
+            onClick={() => props.close()}
+        >
+            <FontAwesomeIcon icon={icons.cancel} />
+        </button>
+    </div>);
+};
+export default NewBoxForm;
