e263e784a4c709ccaa042627d572fdf95850c6e4
James Faix <jamesthefaix@gmail.com>
2022-01-26 11:51:15 -0800

    Got react devtools working

--- src/electron.ts
+++ src/electron.ts
@@ -1,18 +1,59 @@
-const { app, BrowserWindow } = require('electron');
+import * as Electron from 'electron';
+import CrossProcessExports = require('electron');
+import installExtension, { REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } from 'electron-devtools-installer';
 
-function createWindow () {
-    // Create the browser window.
-    const win = new BrowserWindow({
-      width: 1000,
-      height: 1000,
-      webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: false
-      }
-    });  // and load the index.html of the app.
+let mainWindow : CrossProcessExports.BrowserWindow | null;
 
-    win.loadFile('index.html');
-    win.webContents.openDevTools();
+const installExtensions = async () => {
+  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+  const extensions = [REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS];
+  installExtension(
+        extensions,
+        {loadExtensionOptions: {allowFileAccess: true}, forceDownload: forceDownload}
+      )
+      .catch(console.log);
 }
 
-app.on('ready', createWindow);
+function createWindow() {
+  mainWindow = new Electron.BrowserWindow({
+    width: 1000,
+    height: 1000,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  });
+
+  mainWindow.loadFile("index.html");
+
+  mainWindow.on("closed", () => (mainWindow = null));
+}
+
+Electron.app.on("ready", async () => {
+  createWindow();
+
+  // Install extensions
+  await installExtensions();
+
+  // Auto-open dev tools
+  mainWindow?.webContents.on("did-frame-finish-load", () => {
+    mainWindow?.webContents.once("devtools-opened", () => {
+      mainWindow?.focus();
+    });
+    mainWindow?.webContents.openDevTools({
+      mode: 'undocked'
+    });
+  });
+});
+
+Electron.app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    Electron.app.quit();
+  }
+});
+
+Electron.app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
