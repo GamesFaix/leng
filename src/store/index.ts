3f38fbfbd6394a851a7b9f01792048651bc06f27
James Faix <jamesthefaix@gmail.com>
2022-01-26 12:26:53 -0800

    Got redux devtools working

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,12 @@
-import { applyMiddleware, combineReducers, createStore } from '@reduxjs/toolkit';
-import { encyclopediaReducer } from './encyclopedia';
-import { inventoryReducer } from './inventory';
-import logger from 'redux-logger';
-import { settingsReducer } from './settings';
+import { combineReducers, createStore, StoreEnhancer } from '@reduxjs/toolkit';
+import { EncyclopediaAction, EncyclopediaActionTypes, encyclopediaReducer } from './encyclopedia';
+import { InventoryAction, inventoryReducer } from './inventory';
+import { SettingsAction, settingsReducer } from './settings';
+
+type Action =
+  EncyclopediaAction |
+  InventoryAction |
+  SettingsAction
 
 const reducer = combineReducers({
     encyclopedia: encyclopediaReducer,
@@ -10,9 +14,33 @@ const reducer = combineReducers({
     settings: settingsReducer
 });
 
+// Large payloads break the devtools
+// https://github.com/zalmoxisus/redux-devtools-extension/blob/master/docs/Troubleshooting.md#excessive-use-of-memory-and-cpu
+const actionSanitizer =  (action: Action) => {
+  switch (action.type) {
+    case EncyclopediaActionTypes.LoadSuccess:
+      return { ...action, cards: '<<LARGE BLOB>>' };
+    default:
+      return action;
+  }
+};
+
+const stateSanitizer = (state: any /* RootState is defined below */) => {
+  return {
+    ...state,
+    encyclopedia: '<<LARGE BLOB>>'
+  }
+}
+
+const enhancer = (window as any).__REDUX_DEVTOOLS_EXTENSION__
+  && (window as any).__REDUX_DEVTOOLS_EXTENSION__({
+    actionSanitizer,
+    stateSanitizer
+  });
+
 export const store = createStore(
     reducer,
-  //  applyMiddleware(logger)
+    enhancer
 );
 
 export type RootState = ReturnType<typeof store.getState>
