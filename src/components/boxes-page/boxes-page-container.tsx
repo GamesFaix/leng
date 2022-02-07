99f3c0295e1db91ac24fc8748d78135770244e35
James Faix <jamesthefaix@gmail.com>
2022-02-07 09:37:54 -0800

    Implemented preload saga

--- src/components/boxes-page/boxes-page-container.tsx
+++ src/components/boxes-page/boxes-page-container.tsx
@@ -2,27 +2,15 @@ import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../store';
 import { inventoryActions } from '../../store/inventory';
-import LoadingMessage from '../common/loading-message';
 import BoxesPage from './boxes-page';
 
 const BoxesPageContainer = () => {
-    const settings = useSelector((state: RootState) => state.settings.settings);
     const boxes = useSelector((state: RootState) => state.inventory.boxes);
     const dispatch = useDispatch();
     const [isNewBoxFormVisible, setIsNewBoxFormVisible] = React.useState(false);
 
-    React.useEffect(() => {
-        if (settings !== null && boxes === null) {
-            dispatch(inventoryActions.boxInfosLoadStart());
-        }
-    });
-
     function deleteBox(name: string) {
         dispatch(inventoryActions.boxDeleteStart(name));
-    }
-
-    if (!settings || !boxes) {
-        return <LoadingMessage message="Loading box info..."/>;
     }
 
     return (
--- src/sagas/preload.ts
+++ src/sagas/preload.ts
@@ -1,21 +1,76 @@
+import { put, select, takeEvery, takeLeading } from "redux-saga/effects";
+import { AsyncRequestStatus } from "../logic/model";
+import { RootState } from "../store";
+import { encyclopediaActions, EncyclopediaActionTypes, EncyclopediaLoadAction } from "../store/encyclopedia";
+import { BoxInfosLoadAction, BoxLoadAction, BoxState, inventoryActions, InventoryActionTypes } from "../store/inventory";
+import { preloadActions, PreloadActionTypes, PreloadStartAction } from "../store/preload";
+import { settingsActions, SettingsActionTypes, SettingsLoadAction } from "../store/settings";
 
-// function* loadSettings(action: SettingsLoadAction) {
-//     if (action.value.status !== AsyncRequestStatus.Started) {
-//         return;
-//     }
+function* onPreloadStart(action: PreloadStartAction) {
+    yield put(settingsActions.loadStart());
+}
 
-//     try {
-//         const settings = loadSettingsOrDefaults();
-//         yield put(settingsActions.loadSuccess(settings));
+function* onSettingsLoad(action: SettingsLoadAction) {
+    switch (action.value.status) {
+        case AsyncRequestStatus.Started:
+            yield put(preloadActions.update("Loading settings..."));
+            break;
+        case AsyncRequestStatus.Success:
+            yield put(encyclopediaActions.loadStart());
+            break;
+    }
+}
 
-//     }
-//     catch (error) {
-//         yield put(settingsActions.loadError(`${error}`));
-//     }
-// }
+function* onEncyclopediaLoad(action: EncyclopediaLoadAction) {
+    switch (action.value.status) {
+        case AsyncRequestStatus.Started:
+            yield put(preloadActions.update("Loading Scryfall data..."));
+            break;
+        case AsyncRequestStatus.Success:
+            yield put(inventoryActions.boxInfosLoadStart());
+            break;
+    }
+}
+
+function* onBoxInfosLoad(action: BoxInfosLoadAction) {
+    switch (action.value.status) {
+        case AsyncRequestStatus.Started:
+            yield put(preloadActions.update("Loading box metadata..."));
+            break;
+        case AsyncRequestStatus.Success:
+            const boxInfos = action.value.data ?? [];
+            for (let b of boxInfos) {
+                yield put(inventoryActions.boxLoadStart(b.name));
+            }
+            break;
+    }
+}
+
+function* onBoxLoad(action: BoxLoadAction) {
+    switch (action.value.status) {
+        case AsyncRequestStatus.Started: {
+            const boxName = action.value.data;
+            yield put(preloadActions.update(`Loading box ${boxName}...`));
+            break;
+        }
+        case AsyncRequestStatus.Success: {
+            const box = action.value.data;
+            const boxes : BoxState[] = yield select((state: RootState) => state.inventory.boxes);
+            const otherBoxes = boxes.filter(b => b.name !== box.name);
+            const isLastBox = otherBoxes.every(b => b.cards !== null);
+            if (isLastBox) {
+                yield put(preloadActions.finish());
+            }
+            break;
+        }
+    }
+}
 
 function* preloadSaga() {
-    // yield takeLeading(SettingsActionTypes.Load, loadSettings);
-    // yield takeEvery(SettingsActionTypes.Save, saveSettings);
+    yield takeLeading(PreloadActionTypes.Start, onPreloadStart);
+    yield takeEvery(SettingsActionTypes.Load, onSettingsLoad);
+    yield takeEvery(EncyclopediaActionTypes.Load, onEncyclopediaLoad);
+    yield takeEvery(InventoryActionTypes.BoxInfosLoad, onBoxInfosLoad);
+    yield takeEvery(InventoryActionTypes.BoxLoad, onBoxLoad);
 }
 export default preloadSaga;
