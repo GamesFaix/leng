3ead081a6b9dd29d0f8b290638cd451702ae5f12
James Faix <jamesthefaix@gmail.com>
2022-02-12 20:12:08 -0800

    Added tooltip with card details

--- src/components/virtual-binder/binder-card.tsx
+++ src/components/virtual-binder/binder-card.tsx
@@ -1,4 +1,4 @@
-import { Badge, CircularProgress } from '@mui/material';
+import { Badge, CircularProgress, Tooltip, Typography } from '@mui/material';
 import { sumBy } from 'lodash';
 import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -7,10 +7,31 @@ import { getCardImagePath } from '../../sagas/encyclopedia';
 import { RootState } from '../../store';
 import { encyclopediaActions } from '../../store/encyclopedia';
 import selectors from '../../store/selectors';
+import FlagIcon from './flag-icon';
 
 type Props = {
     cardGroup: BoxCard[], // Group of cards with same name/set/collectors number, but different foil or language
     style?: React.CSSProperties
+}
+
+function getCardLabel(card: BoxCard) {
+    const foil = card.foil ? ' Foil' : '';
+    return <React.Fragment key={card.scryfallId}>
+        <br/>
+        <span>
+            {card.count} <FlagIcon lang={card.lang}/> {foil}
+        </span>
+    </React.Fragment>;
+}
+
+function getTooltip(cardGroup: BoxCard[])    {
+    const card = cardGroup[0];
+
+    return (<Typography>
+        {card.name}<br/>
+        {`${card.setName} (#${card.collectorsNumber})`}
+        {cardGroup.map(getCardLabel)}
+    </Typography>);
 }
 
 const BinderCard = (props: Props) => {
@@ -27,35 +48,41 @@ const BinderCard = (props: Props) => {
         }
     })
 
-    // TODO: Display count
-
     const count = sumBy(props.cardGroup, c => c.count);
+    const tooltip = getTooltip(props.cardGroup);
 
     return (
-        <Badge
-            color="secondary"
-            badgeContent={count !== 1 ? count : undefined}
-            overlap="circular"
+        <Tooltip
+            title={
+                <span style={{ whiteSpace: 'pre-line' }}>
+                    {tooltip}
+                </span>
+            }
         >
-            <div
-                style={{
-                    ...props.style,
-                    overflow: 'clip'
-                }}
-                title={`${card.name} - ${card.set_name} (${card.collector_number})`}
+            <Badge
+                color="secondary"
+                badgeContent={count}
+                overlap="circular"
             >
-                {isImageLoaded
-                    ? <img
-                        src={imagePath}
-                        style={{
-                            maxWidth: '100%',
-                            maxHeight: '100%'
-                        }}
-                    />
-                    // TODO: Center spinner
-                    : <CircularProgress/> }
-            </div>
-        </Badge>
+                <div
+                    style={{
+                        ...props.style,
+                        overflow: 'clip'
+                    }}
+                >
+                    {isImageLoaded
+                        ? <img
+                            src={imagePath}
+                            style={{
+                                maxWidth: '100%',
+                                maxHeight: '100%'
+                            }}
+                        />
+                        // TODO: Center spinner
+                        : <CircularProgress/> }
+                </div>
+            </Badge>
+        </Tooltip>
     )
 }
 export default BinderCard;
