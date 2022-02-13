fc9ef2dcadf4deaec1562f2d826697bd56737521
James Faix <jamesthefaix@gmail.com>
2022-02-12 21:51:11 -0800

    Grouping tokens, promos, art together

--- src/components/virtual-binder/binder.tsx
+++ src/components/virtual-binder/binder.tsx
@@ -1,5 +1,5 @@
 import { Card } from '@mui/material';
-import { chunk, groupBy, orderBy } from 'lodash';
+import { chunk, Dictionary, groupBy, orderBy } from 'lodash';
 import * as React from 'react';
 import { useSelector } from 'react-redux';
 import { Grid, GridCellProps } from 'react-virtualized';
@@ -13,10 +13,6 @@ const scale = 60;
 
 type Props = {
     cards: BoxCard[]
-}
-
-function compareCards(a: BoxCard, b: BoxCard) {
-    return compareCollectorsNumbers(a.collectorsNumber, b.collectorsNumber);
 }
 
 function compareCollectorsNumbers(a: string, b: string) : number {
@@ -42,33 +38,92 @@ function compareCollectorsNumbers(a: string, b: string) : number {
     return 0;
 }
 
-function organizePages(cards: BoxCard[], sets: Set[]) : BoxCard[][][] { // An array of pages, which are arrays of card groups, which are arrays of foil/non-foil versions of the same card
-    const groupedBySet = groupBy(cards, c => c.setAbbrev);
+enum SetType {
+    Normal,
+    Tokens,
+    Promos,
+    ArtSeries
+}
 
-    const setsWithCards = innerJoin(
+function getSetType(setName: string) : SetType {
+    if (setName.endsWith(' Tokens')) return SetType.Tokens;
+    if (setName.endsWith(' Promos')) return SetType.Promos;
+    if (setName.endsWith(' Art Series')) return SetType.ArtSeries;
+    return SetType.Normal;
+}
+
+function getSetBase(setName: string) : string {
+    if (setName.endsWith(' Tokens')
+    || setName.endsWith(' Promos')) {
+        return setName.substring(0, setName.length - 7);
+    }
+    if (setName.endsWith(' Art Series')) {
+        return setName.substring(0, setName.length - 11);
+    }
+
+    return setName;
+}
+
+type SetWithCards = {
+    set: Set,
+    cards: BoxCard[]
+}
+
+type SetWithCardGroups = {
+    set: Set,
+    cardGroups: BoxCard[][]
+}
+
+function organizePages(cards: BoxCard[], sets: Set[]) : BoxCard[][][] { // An array of pages, which are arrays of card groups, which are arrays of foil/non-foil versions of the same card
+    const groupedBySet : Dictionary<BoxCard[]> = groupBy(cards, c => c.setAbbrev);
+
+    const setsWithCards : SetWithCards[] = innerJoin(
         sets,
         Object.entries(groupedBySet),
         set => set.code,
         grp => grp[0],
-        (set, grp) => [set, grp[1]]);
+        (set, grp) => { return { set, cards: grp[1] }; }
+    );
 
-    const setsWithPages = setsWithCards
-        .map(tup => {
-            const set = tup[0] as Set;
-            const cards = tup[1] as BoxCard[];
-            const groupById = groupBy(cards, c => c.scryfallId);
+    const setsWithSortedCardGroups : SetWithCardGroups[] =
+        setsWithCards.map(x => {
+            const groupById = groupBy(x.cards, c => c.scryfallId);
             const sorted = Object.values(groupById).sort((a, b) => compareCollectorsNumbers((a[0].collectorsNumber), (b[0].collectorsNumber)));
-            return [set, chunk(sorted, 9)];
+            return {
+                set: x.set,
+                cardGroups: sorted
+            };
+        })
+
+    const groupSetsWithCardsBySetBase : Dictionary<SetWithCardGroups[]> =
+        groupBy(setsWithSortedCardGroups, x => getSetBase(x.set.name));
+
+    const relatedSetsWithCardGroups = Object.values(groupSetsWithCardsBySetBase)
+        .map(grp => {
+            const withTypes = grp.map(x => { return { ...x, type: getSetType(x.set.name)}; });
+            const baseSet = withTypes.find(x => x.type === SetType.Normal);
+            const tokenSet = withTypes.find(x => x.type === SetType.Tokens);
+            const promosSet = withTypes.find(x => x.type === SetType.Promos);
+            const artSeriesSet = withTypes.find(x => x.type === SetType.ArtSeries);
+
+            const set = baseSet?.set ?? grp[0].set; // Some promo sets don't have a base set
+            const cardGroups = (promosSet?.cardGroups ?? [])
+                .concat(baseSet?.cardGroups ?? [])
+                .concat(tokenSet?.cardGroups ?? [])
+                .concat(artSeriesSet?.cardGroups ?? []);
+
+            return { set, cardGroups };
         });
+
+    const setsWithPages =
+        relatedSetsWithCardGroups
+            .map(x => { return { set: x.set, pages: chunk(x.cardGroups, 9)}; });
 
     const sortedSets =
-        orderBy(setsWithPages, tup => {
-            const set = tup[0] as Set;
-            return set.released_at;
-        });
+        orderBy(setsWithPages, x => x.set.released_at);
 
     const pages = sortedSets
-        .map(tup => tup[1] as BoxCard[][][])
+        .map(x => x.pages)
         .reduce((a,b) => a.concat(b), []);
 
     return pages;
