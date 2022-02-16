f62c5b5d6dc14627071c150a13327ecca37a9ec7
James Faix <jamesthefaix@gmail.com>
2022-02-15 20:24:05 -0800

    Missing from binder report (#90)
    * Created "stuff missing from binders" report
    
    * Moved missing report to new tab

--- src/components/reports-page/reports-page.tsx
+++ src/components/reports-page/reports-page.tsx
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import selectors from '../../store/selectors';
 import TabPanel from '../common/tab-panel';
 import BinderBySetReport from './binder-by-set-report';
+import MissingFromBinderReport from './missing-from-binders-report';
 
 const ReportsPage = () => {
     const [tabId, setTabId] = React.useState(0);
@@ -15,17 +16,13 @@ const ReportsPage = () => {
             onChange={(e, tabId) => setTabId(tabId)}
         >
             <Tab label="Sets Binder"/>
-            <Tab label="Tab 2"/>
-            <Tab label="Tab 3"/>
+            <Tab label="Missing from Binders"/>
         </Tabs>
         <TabPanel hidden={tabId !== 0}>
             <BinderBySetReport boxes={boxes} />
         </TabPanel>
         <TabPanel hidden={tabId !== 1}>
-            <span>Tab 2 contents</span>
-        </TabPanel>
-        <TabPanel hidden={tabId !== 2}>
-            <span>Tab 3 contents</span>
+            <MissingFromBinderReport />
         </TabPanel>
     </div>);
 }
