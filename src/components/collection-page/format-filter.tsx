b8d45fc52396dd30af1148ed786a0fc7301bea16
James Faix <jamesthefaix@gmail.com>
2022-05-07 13:33:03 -0700

    Sorted and formatted format names

--- src/components/collection-page/format-filter.tsx
+++ src/components/collection-page/format-filter.tsx
@@ -3,13 +3,70 @@ import * as React from 'react';
 import { useSelector } from 'react-redux';
 import selectors from '../../store/selectors';
 
+type FormatName = string | null;
+
 type Props = {
-    value: string | null,
-    onChange: (value: string | null) => void
+    value: FormatName,
+    onChange: (value: FormatName) => void
+}
+
+function capitalize(str: string) : string {
+    switch (str.length) {
+        case 0: return str;
+        case 1: return str.toUpperCase();
+        default:
+            return `${str.charAt(0).toUpperCase()}${str.substring(1)}`;
+    }
+}
+
+function toDisplayName(format: FormatName) : string {
+    switch (format) {
+        case null: return "(Casual)";
+        case "historicbrawl": return "Historic Brawl";
+        case "paupercommander": return "Pauper Commander";
+        case "oldschool": return "Old School";
+        case "premodern": return "Pre-modern";
+        default: return capitalize(format);
+    }
+}
+
+const prioritizedFormats : FormatName[] = [
+    null,
+    "commander",
+    "standard",
+    "pioneer",
+    "modern",
+    "legacy",
+    "vintage",
+    "premodern",
+    "oldschool",
+    "pauper"
+];
+
+function getSortRank(format: FormatName) : number {
+    const priorityIndex = prioritizedFormats.indexOf(format);
+    return priorityIndex >= 0 ? priorityIndex : 100;
+}
+
+function sortFormats(formats: FormatName[]) : FormatName[] {
+    const withRanks = formats.map(f => { return { format: f, rank: getSortRank(f) }});
+
+    const sorted = withRanks
+        .sort((a, b) => {
+            if (a.rank < b.rank) return -1;
+            if (a.rank > b.rank) return 1;
+            if ((a.format ?? "") < (b.format ?? "")) return -1;
+            if ((a.format ?? "") > (b.format ?? "")) return 1;
+            return 0;
+        });
+
+    return sorted.map(x => x.format);
 }
 
 const FormatFilter = (props: Props) => {
-    const formats = useSelector(selectors.formats);
+    let formats : FormatName[] = [ null ];
+    formats = formats.concat(useSelector(selectors.formats));
+    formats = sortFormats(formats);
 
     return (
         <Select
@@ -17,14 +74,14 @@ const FormatFilter = (props: Props) => {
                 width: 200
             }}
             value={props.value}
-            onChange={e => props.onChange(e.target.value as any)}
+            onChange={e => props.onChange(e.target.value ?? null)}
         >
             {formats.map(f =>
                 <MenuItem
-                    key={f}
-                    value={f}
+                    key={f ?? "(null)"}
+                    value={f ?? undefined}
                 >
-                    {f}
+                    {toDisplayName(f)}
                 </MenuItem>
             )}
         </Select>
