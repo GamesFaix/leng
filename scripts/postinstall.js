b1aa628487bdb33a42a02df64f18aafa5b60dbfe
James Faix <jamesthefaix@gmail.com>
2023-04-19 21:52:30 -0700

    Remove redundant postinstall in build pipeline

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,4 +1,7 @@
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require('fs');
+console.log('POST-INSTALL');
+
+console.log(`Deleting node_modules/@types/react-virtualized/node_modules`);
 fs.rmSync(`${__dirname}/../node_modules/@types/react-virtualized/node_modules`, { recursive: true });
