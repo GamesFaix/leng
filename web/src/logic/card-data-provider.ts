96d8a6b8bdfa64f3913010076816bee65562d3d8
James Faix <jamesthefaix@gmail.com>
2023-11-10 01:01:18 -0800

    Force caching json requests

--- web/src/logic/card-data-provider.ts
+++ web/src/logic/card-data-provider.ts
@@ -6,7 +6,7 @@ const dir = "../../data/encyclopedia";
 const getAllCards = async (): Promise<Card[]> => {
   const path = `${dir}/cards.json`;
   console.log("getAllCards");
-  const json = await fetch(path);
+  const json = await fetch(path, { cache: "force-cache" });
   console.log("decoding card JSON...");
   const data: Card[] = await json.json();
   console.log(`decoded ${data.length} cards`);
@@ -16,7 +16,7 @@ const getAllCards = async (): Promise<Card[]> => {
 const getAllSets = async (): Promise<Set[]> => {
   const path = `${dir}/sets.json`;
   console.log(path);
-  const json = await fetch(path);
+  const json = await fetch(path, { cache: "force-cache" });
   console.log("decoding set JSON...");
   const data: Set[] = await json.json();
   console.log(`decoded ${data.length} sets`);
