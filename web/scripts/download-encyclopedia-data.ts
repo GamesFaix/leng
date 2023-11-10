fd66078dee06708129cc4b2a6060518bea3e0c6d
James Faix <jamesthefaix@gmail.com>
2023-11-10 00:03:20 -0800

    Deploy pipeline (#129)
    * Fix bad usage of path package
    
    * Updated download encyclopedia script
    
    * Add deploy pipeline based on eccentricenter
    
    * Make deploy manual
    
    * Update deploy.yml

--- web/scripts/download-encyclopedia-data.ts
+++ web/scripts/download-encyclopedia-data.ts
@@ -1,13 +1,14 @@
 import { writeFileSync, existsSync, mkdirSync } from "fs";
-import { resolve, dirname } from "path";
+import { resolve } from "path";
 import { getAllCards, getAllSets } from "leng-core/src/domain/scryfall";
 
-const dir = dirname(resolve(__dirname, `../dist/data/encyclopedia`));
+const dir = "./dist/data/encyclopedia";
 
 const downloadCards = async () => {
   console.log("Downloading card data...");
   const cards = await getAllCards();
-  const path = resolve(dir, "cards.json");
+  console.log(`Found ${cards.length} cards.`);
+  const path = resolve(`${dir}/cards.json`);
   console.log(`Saving card data to ${path}...`);
   const json = JSON.stringify(cards);
   writeFileSync(path, json);
@@ -16,13 +17,15 @@ const downloadCards = async () => {
 const downloadSets = async () => {
   console.log("Downloading set data...");
   const sets = await getAllSets();
-  const path = resolve(dir, "sets.json");
+  console.log(`Found ${sets.length} sets.`);
+  const path = resolve(`${dir}/sets.json`);
   console.log(`Saving set data to ${path}...`);
   const json = JSON.stringify(sets);
   writeFileSync(path, json);
 };
 
 const downloadData = async () => {
+  console.log(`Downloading encyclopedia to ${resolve(dir)}...`);
   if (!existsSync(dir)) {
     mkdirSync(dir);
   }
