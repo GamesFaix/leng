f2bdcb763e37d04bb3dccad1d19665a121fdcdda
James Faix <jamesthefaix@gmail.com>
2023-11-12 00:58:36 -0800

    Use last-modified header

--- web/src/logic/card-data-provider.ts
+++ web/src/logic/card-data-provider.ts
@@ -3,25 +3,36 @@ import { Card, Set } from "leng-core/src/domain/encyclopedia";
 
 const dir = "../../data/encyclopedia";
 
-const getAllCards = async (): Promise<Card[]> => {
-  const path = `${dir}/cards.json`;
-  console.log("getAllCards");
-  const json = await fetch(path, { cache: "force-cache" });
-  console.log("decoding card JSON...");
-  const data: Card[] = await json.json();
-  console.log(`decoded ${data.length} cards`);
+const loadFile = async <T>(file: string): Promise<T[]> => {
+  const path = `${dir}/${file}.json`;
+  console.log(`loadFile ${file}`);
+
+  const storageKey = `encyclopedia-${file}-last-modified`;
+
+  let lastModified = localStorage.getItem(storageKey);
+  const request = new Request(path);
+  if (lastModified) { request.headers.set("If-Modified-Since", lastModified)}
+
+  const response = await fetch(request);
+  
+  lastModified = response.headers.get("last-modified");
+  if (lastModified) {
+    console.log("updating last modified date")
+    localStorage.setItem(storageKey, lastModified);    
+  }
+  
+  console.log(`decoding ${file} JSON...`);
+  const data: T[] = await response.json();
+
+  console.log(`decoded ${data.length} ${file}`);
   return data;
 };
 
-const getAllSets = async (): Promise<Set[]> => {
-  const path = `${dir}/sets.json`;
-  console.log(path);
-  const json = await fetch(path, { cache: "force-cache" });
-  console.log("decoding set JSON...");
-  const data: Set[] = await json.json();
-  console.log(`decoded ${data.length} sets`);
-  return data;
-};
+const getAllCards = async (): Promise<Card[]> => 
+  loadFile("cards");
+
+const getAllSets = async (): Promise<Set[]> => 
+  loadFile("sets");
 
 export const cardDataProvider: CardDataProvider = {
   getAllCards,
