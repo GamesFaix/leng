0d0c7b60e92bae6977e31d606ed83127f822ff72
James Faix <jamesthefaix@gmail.com>
2023-11-21 03:14:07 -0800

    Refactored binder components

--- core/src/components/virtual-binder/binder-card.tsx
+++ core/src/components/virtual-binder/binder-card.tsx
@@ -1,39 +1,38 @@
 import { Badge, Tooltip } from "@mui/material";
 import { sumBy } from "lodash";
 import { BoxCard } from "../../domain/inventory";
-import BinderCardTooltip from "./binder-card-tooltip";
+import { BinderCardTooltip } from "./binder-card-tooltip";
 import { CardImage } from "../common";
-import { CSSProperties } from "react";
+import { CSSProperties, FC } from "react";
 
 type Props = {
   cardGroup: BoxCard[]; // Group of cards with same name/set/collectors number, but different foil or language
   style?: CSSProperties;
 };
 
-const BinderCard = (props: Props) => {
-  const count = sumBy(props.cardGroup, (c) => c.count);
-  const multipleVersionSignifier = props.cardGroup.length > 1 ? "*" : "";
+export const BinderCard: FC<Props> = ({ cardGroup, style }) => {
+  const count = sumBy(cardGroup, (c) => c.count);
+  const multipleVersionSignifier = cardGroup.length > 1 ? "*" : "";
   const badgeContent = `${count}${multipleVersionSignifier}`;
 
   return (
     <Tooltip
       title={
         <span style={{ whiteSpace: "pre-line" }}>
-          <BinderCardTooltip cardGroup={props.cardGroup} />
+          <BinderCardTooltip cardGroup={cardGroup} />
         </span>
       }
     >
       <Badge color="secondary" badgeContent={badgeContent} overlap="circular">
         <div
           style={{
-            ...props.style,
+            ...style,
             overflow: "clip",
           }}
         >
-          <CardImage scryfallId={props.cardGroup[0].scryfallId} />
+          <CardImage scryfallId={cardGroup[0].scryfallId} />
         </div>
       </Badge>
     </Tooltip>
   );
 };
-export default BinderCard;
--- core/src/components/virtual-binder/binder-page.tsx
+++ core/src/components/virtual-binder/binder-page.tsx
@@ -1,7 +1,7 @@
-import { chunk } from 'lodash';
+import { chunk } from "lodash";
 import { BoxCard } from "../../domain/inventory";
-import BinderCard from './binder-card';
-import { CSSProperties } from 'react';
+import { BinderCard } from "./binder-card";
+import { CSSProperties, FC } from "react";
 
 const cardAspectRatio = [2.5, 3.5];
 const rowsPerPage = 3;
@@ -10,52 +10,52 @@ const colsPerPage = 3;
 const cardMargin = 3;
 
 type Props = {
-    cardGroups: BoxCard[][],
-    style: CSSProperties,
-    scale: number
-}
+  cardGroups: BoxCard[][];
+  style: CSSProperties;
+  scale: number;
+};
 
-const BinderPage = (props: Props) => {
-    const cardHeight = cardAspectRatio[1] * props.scale;
-    const cardWidth = cardAspectRatio[0] * props.scale;
-    const pageHeight = (cardHeight * rowsPerPage) + (cardMargin * 2 * rowsPerPage);
-    const pageWidth =  (cardWidth * colsPerPage) + (cardMargin * 2 * colsPerPage);
-    const rowsData = chunk(props.cardGroups, colsPerPage);
+export const BinderPage: FC<Props> = ({ cardGroups, style, scale }) => {
+  const cardHeight = cardAspectRatio[1] * scale;
+  const cardWidth = cardAspectRatio[0] * scale;
+  const pageHeight = cardHeight * rowsPerPage + cardMargin * 2 * rowsPerPage;
+  const pageWidth = cardWidth * colsPerPage + cardMargin * 2 * colsPerPage;
+  const rowsData = chunk(cardGroups, colsPerPage);
 
-    return (
+  return (
+    <div
+      style={{
+        ...style,
+        width: `${pageWidth}px`,
+        height: `${pageHeight}px`,
+        backgroundColor: "#333333",
+        display: "flex",
+        flexDirection: "column",
+        padding: "3px",
+        borderRadius: "5px",
+      }}
+    >
+      {rowsData.map((cs, row) => (
         <div
-            style={{
-               ...props.style,
-                width: `${pageWidth}px`,
-                height: `${pageHeight}px`,
-                backgroundColor: '#333333',
-                display: 'flex',
-                flexDirection: 'column',
-                padding: '3px',
-                borderRadius: '5px'
-            }}
+          key={row}
+          style={{
+            display: "flex",
+          }}
         >
-            {rowsData.map((cs,row) =>
-                <div key={row}
-                    style={{
-                        display: 'flex'
-                    }}
-                >
-                    {cs.map((c,col) =>
-                        <BinderCard
-                            key={col}
-                            cardGroup={c}
-                            style={{
-                                width: `${cardWidth}px`,
-                                height: `${cardHeight}px`,
-                                borderRadius: '5px',
-                                margin: `${cardMargin}px`
-                            }}
-                        />
-                    )}
-                </div>
-            )}
+          {cs.map((c, col) => (
+            <BinderCard
+              key={col}
+              cardGroup={c}
+              style={{
+                width: `${cardWidth}px`,
+                height: `${cardHeight}px`,
+                borderRadius: "5px",
+                margin: `${cardMargin}px`,
+              }}
+            />
+          ))}
         </div>
-    );
-}
-export default BinderPage;
+      ))}
+    </div>
+  );
+};
