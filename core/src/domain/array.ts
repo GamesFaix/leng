9dc9b6d5111a19897071d3fa38565d8c40b3ab16
James Faix <jamesthefaix@gmail.com>
2024-01-27 21:20:56 -0800

    Stop breaking when a card ID has been replaced by Scryfall (#159)

--- core/src/domain/array.ts
+++ core/src/domain/array.ts
@@ -1,23 +1,27 @@
 import { groupBy } from "lodash";
 
 export function innerJoin<TLeft, TRight, TResult>(
-    left: TLeft[],
-    right: TRight[],
-    leftSelector: (l:TLeft) => string,
-    rightSelector: (r:TRight) => string,
-    resultSelector: (l:TLeft, r:TRight) => TResult
-) : TResult[] {
-    const rightCache = groupBy(right, rightSelector);
-    return left
-        .map(l => {
-            const key = leftSelector(l);
-            const matches = rightCache[key];
-            if (matches) {
-                return matches.map(r => resultSelector(l,r))
-            }
-            else {
-                return [];
-            }
-        })
-        .reduce((a,b) => a.concat(b), []);
+  left: TLeft[],
+  right: TRight[],
+  leftSelector: (l: TLeft) => string,
+  rightSelector: (r: TRight) => string,
+  resultSelector: (l: TLeft, r: TRight) => TResult
+): TResult[] {
+  const rightCache = groupBy(right, rightSelector);
+  return left
+    .map((l) => {
+      const key = leftSelector(l);
+      const matches = rightCache[key];
+      if (matches) {
+        return matches.map((r) => resultSelector(l, r));
+      } else {
+        return [];
+      }
+    })
+    .reduce((a, b) => a.concat(b), []);
 }
+
+export const choose = <TSource, TResult>(
+  xs: TSource[],
+  selector: (x: TSource) => TResult | null
+): TResult[] => xs.map(selector).filter((x) => x !== null) as TResult[];
--- core/src/domain/inventory-files/logic.ts
+++ core/src/domain/inventory-files/logic.ts
@@ -2,57 +2,66 @@ import { CardFinish, CardIndex, Language, getVersionLabel } from "../encyclopedi
 import { Box, BoxInfo } from "../inventory";
 import { normalizeName } from "../names";
 import { FileBox } from "./types";
+import { choose } from "../array";
 
 const getFinish = (finish: CardFinish, foil: boolean): CardFinish => {
-    if (finish) return finish;
-    return foil ? CardFinish.Foil : CardFinish.Normal;
-  };
-  
-  export const fromFileBox = (fileBox: FileBox, cardIndex: CardIndex): Box => ({
+  if (finish) return finish;
+  return foil ? CardFinish.Foil : CardFinish.Normal;
+};
+
+export const fromFileBox = (fileBox: FileBox, cardIndex: CardIndex): Box => {
+  const cards = choose(fileBox.cards, (c) => {
+    const match = cardIndex[c.scryfallId];
+    if (!match) {
+      console.warn(`Inventory card missing in Scryfall data. ID:${c.scryfallId} (${c.name} - ${c.setAbbrev})`);
+      return null;
+    }
+
+    return {
+      name: c.name,
+      setAbbrev: c.setAbbrev,
+      setName: match?.set_name ?? "",
+      scryfallId: c.scryfallId,
+      lang: c.lang ?? Language.English,
+      finish: getFinish(c.finish, c.foil),
+      collectorsNumber: c.collectorsNumber ?? match.num,
+      count: c.count,
+      color: match?.colors ?? [],
+      colorIdentity: match?.color_identity ?? [],
+      normalizedName: normalizeName(c.name),
+      versionLabel: match ? getVersionLabel(match) : "",
+      legalities: match.legalities,
+      type: match.type,
+    };
+  });
+
+  return {
     name: fileBox.name,
     lastModified: fileBox.lastModified,
     description: fileBox.description,
-    cards: fileBox.cards.map((c) => {
-      const match = cardIndex[c.scryfallId];
-      return {
-        name: c.name,
-        setAbbrev: c.setAbbrev,
-        setName: match?.set_name ?? "",
-        scryfallId: c.scryfallId,
-        lang: c.lang ?? Language.English,
-        finish: getFinish(c.finish, c.foil),
-        collectorsNumber: c.collectorsNumber ?? match.num,
-        count: c.count,
-        color: match?.colors ?? [],
-        colorIdentity: match?.color_identity ?? [],
-        normalizedName: normalizeName(c.name),
-        versionLabel: match ? getVersionLabel(match) : "",
-        legalities: match.legalities,
-        type: match.type
-      };
-    }),
-  });
-  
-  export const toFileBox = (box: Box): FileBox => ({
-    name: box.name,
-    lastModified: box.lastModified,
-    description: box.description,
-    cards: box.cards.map((c) => {
-      return {
-        name: c.name,
-        setAbbrev: c.setAbbrev,
-        scryfallId: c.scryfallId,
-        lang: c.lang,
-        finish: c.finish,
-        foil: c.finish !== CardFinish.Normal,
-        collectorsNumber: c.collectorsNumber,
-        count: c.count,
-      };
-    }),
-  });
-  
-  export const toBoxInfo = (box: FileBox): BoxInfo => ({
-    name: box.name,
-    lastModified: box.lastModified,
-  });
-  
+    cards,
+  };
+};
+
+export const toFileBox = (box: Box): FileBox => ({
+  name: box.name,
+  lastModified: box.lastModified,
+  description: box.description,
+  cards: box.cards.map((c) => {
+    return {
+      name: c.name,
+      setAbbrev: c.setAbbrev,
+      scryfallId: c.scryfallId,
+      lang: c.lang,
+      finish: c.finish,
+      foil: c.finish !== CardFinish.Normal,
+      collectorsNumber: c.collectorsNumber,
+      count: c.count,
+    };
+  }),
+});
+
+export const toBoxInfo = (box: FileBox): BoxInfo => ({
+  name: box.name,
+  lastModified: box.lastModified,
+});
